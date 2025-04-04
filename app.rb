f2632e88721cabc8ada65640c8fbcdafa02addf5
Kseniia Kormalova <kormalovak@gmail.com>
2025-04-04 23:57:32 +0000

    good

--- app.rb
+++ app.rb
@@ -52,3 +52,18 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dice/:num_of_rolls/:num_of_sides") do
+  @num_dice = params.fetch("num_of_rolls").to_i
+  
+  @sides = params.fetch("num_of_sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do 
+    die = rand(1..@sides)
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
